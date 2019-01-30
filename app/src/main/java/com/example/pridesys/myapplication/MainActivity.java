405365c0b4eecfe1ff9e74e62f493021402d1f0d
shohelrana <shohel.rana@pridesys.com>
2019-01-30 14:58:03 +0600

    My first android application

---
+++ app/src/main/java/com/example/pridesys/myapplication/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.pridesys.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private WebView webView = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        this.webView = (WebView) findViewById(R.id.webview);
+
+        WebSettings webSettings = webView.getSettings();
+
+        webSettings.setJavaScriptEnabled(true);
+
+        WebViewClientImpl webViewClient = new WebViewClientImpl(this);
+
+        webView.setWebViewClient(webViewClient);
+
+        webView.loadUrl("http://www.shohel.net");
+
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && this.webView.canGoBack()) {
+            this.webView.goBack();
+            return true;
+        }
+
+        return super.onKeyDown(keyCode, event);
+    }
+}
